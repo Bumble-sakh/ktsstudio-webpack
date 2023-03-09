e8f1f0bd1b8b0ce7470b6314e16cf4c8c91fcd8c
Bumble <bumble.sakh@gmail.com>
2023-03-10 07:52:09 +1100

    refactor: babel config

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,17 @@ module.exports = (api) => {
   api.cache.using(() => process.env.NODE_ENV);
 
   const isProd = process.env.NODE_ENV === 'production';
-  const presets = ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'];
-  const plugins = [!isProd && 'react-refresh/babel'].filter(Boolean);
+  const presets = [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
+    'mobx',
+  ];
+  const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
+    ['module:fast-async', { spec: true }],
+    !isProd && 'react-refresh/babel',
+  ].filter(Boolean);
 
   return {
     presets,
